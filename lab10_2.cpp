631419ad763ea17add4090c52e380922b6f487f3
tie <tie.teiroom1234@gmail.com>
2024-12-20 15:24:08 +0700

    done

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,9 +6,17 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
-	
+	string line;
+
+	dest << "-------------------- BOOM ---------------------\n";
+
+	while(getline(source,line)){
+		dest << line << "\n";
+	}
+
+	dest << "-------------------- HA!! ---------------------\n";
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
 
